b52f217c383b70df88e2937e2cd5cb338037eafd
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2020-06-15 11:33:01 +0100

    initial commit

---
+++ rotate.js
@@ -0,0 +1,15 @@
+document.addEventListener("mousemove", function (event) {
+  const x = event.pageX
+  const y = event.pageY
+  
+  const midX = x - window.innerWidth / 2
+  const midY = y - window.innerHeight / 2
+  
+  const box = document.querySelector("section")
+  
+ 	box.style.left = x + "px"
+  box.style.top = y + "px"
+  
+	box.style.transform = "rotateX("+ (midY * 0.5) +"deg) rotateY(" + (midX * 0.5) + "deg)"
+  
+})
